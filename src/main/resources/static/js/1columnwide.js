aafcfc2775f08d477736aa14ae05387dd6c6ba04
rahul <rahul.raje@gmail.com>
2016-06-20 20:37:15 +1200

    Added UI Router for nested views

--- src/main/resources/static/js/1columnwide.js
+++ src/main/resources/static/js/1columnwide.js
@@ -1,15 +1,26 @@
-angular.module('1columnwide', [ 'ngRoute', 'ui.bootstrap', 'ngResource' ]).config(function($routeProvider, $httpProvider) {
-    var viewBase = '/js/views/';
+angular.module('1columnwide', [ 'ngRoute', 'ui.router', 'ui.bootstrap', 'ngResource' ])
+		.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+    
+	var viewBase = '/js/views/';
+    var partialsBase = '/js/partials/';
+	// For any unmatched url, redirect to /login
+	$urlRouterProvider.otherwise(viewBase + "/login");
 
-	$routeProvider.when('/login', {
-		templateUrl : viewBase + 'home/login.html',
-		controller : 'login',
-		controllerAs: 'vm'
-	}).when('/home', {
-		controller : 'home',
-        templateUrl: viewBase + 'home/home.html',
-        controllerAs: 'vm'
-	}).otherwise({ redirectTo: '/login' });
+	$stateProvider.state('login', {
+	  url: "/login",
+	  templateUrl: partialsBase + "home/login.html"
+      controller: 'login'
+	})
+    .state('home', {
+      url: "/home",
+      templateUrl: partialsBase + "home/home.html",
+      controller: 'home'
+    })
+    .state('home.search', {
+      url: "/home-search",
+      templateUrl: partialsBase + "home/search-tags.html",
+      controller: 'tagsTypeAhead'
+    });
 
 	$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
